2fa8314090fa3fd545fbb1c3fc5710e8d304bfd4
Lucas Barão <lucas.baraom@gmail.com>
2024-03-01 16:11:53 -0300

    trabalhando com arrays

---
+++ lists/lists.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+	precos := []float64{1, 2, 3}
+	fmt.Println(precos[1])
+	precos[1] = 9.99
+
+	precos = append(precos, 5.99)
+	precos = precos[1:]
+	fmt.Println(precos)
+}
+
+//func main() {
+//	var produtos = [4]string{"Livro"}
+//	precos := [4]float64{1.2, 3.4, 5.6, 5}
+//	fmt.Println(precos)
+//
+//	produtos[2] = "Tapete"
+//
+//	fmt.Println(produtos)
+//
+//	fmt.Println(precos[2])
+//
+//	novosPrecos := precos[1:3]
+//	//novosPrecos := precos[:3]
+//	//novosPrecos := precos[1:]
+//
+//	fmt.Println(novosPrecos)
+//}
